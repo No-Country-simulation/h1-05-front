93bfa5e0368010b847d750559a7ffab8a862c5c7
Felipe <felipe.calderon321@gmail.com>
2024-07-22 11:40:50 -0400

    added style responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,9 +2,9 @@ import MenuDoctor from '@/components/doctor/menu-doctor'
 
 export default function LayoutDashboardMedical({ children }: { children: React.ReactNode }) {
     return (
-        <div>
+        <div className='min-h-screen md:grid md:grid-cols-[1fr_3fr] md:gap-4 md:ml-4 md:pt-4'>
             <MenuDoctor />
-            {children}
+            <div>{children}</div>
         </div>
     )
 }
--- src/components/doctor/header-doctor.tsx
+++ src/components/doctor/header-doctor.tsx
@@ -5,10 +5,10 @@ import { Image } from '@nextui-org/react'
 export default function HeaderDoctor({ medico }: { medico: Medico }) {
     const { diaNombre, diaNumero, mesNombre } = dateFormat()
     return (
-        <div className='bg-[url("/img/samples/header-dashboard-doctor.jpg")] bg-cover flex flex-col md:flex-row items-center justify-between shadow-md rounded-b-lg mb-6 text-white'>
-            <div className='relative bg-black bg-opacity-50 flex gap-3 w-full md:w-fit justify-between items-center md:flex-row-reverse p-4'>
-                <div className='absolute inset-0'></div>
-                <div className='relative z-10'>
+        <div className='relative header-doctor flex flex-col md:flex-row items-center justify-between shadow-md mb-6 text-white'>
+            <div className='absolute inset-0 bg-black/60 z-10'></div>
+            <div className='flex gap-3 w-full md:w-fit justify-between items-center md:flex-row-reverse p-4 z-20'>
+                <div>
                     <h1 className='text-3xl font-bold'>
                         Buen día, <br />
                         Dr. {medico.lastname}
@@ -17,19 +17,18 @@ export default function HeaderDoctor({ medico }: { medico: Medico }) {
                 </div>
                 <Image src={medico.img} alt='Doctor' className='object-cover w-28 relative z-10' />
             </div>
-            <div className='w-full md:w-fit bg-black/50 md:bg-transparent p-4'>
-                <h3 className='w-fit bg-black/70 rounded-full px-2 mx-auto mr-auto md:mr-0 md:ml-auto'>Turnos:</h3>
+            <div className='w-full md:w-fit md:bg-transparent p-4 z-20'>
                 <div className='flex gap-6 md:gap-2 justify-evenly'>
-                    <div className='text-center bg-black/50 rounded-md p-2'>
-                        <p className='text-lg font-semibold'>Pendientes</p>
+                    <div className='text-center w-28 bg-purple-900/60 rounded-full p-2'>
+                        <p className='text-xs font-semibold uppercase'>Pendientes</p>
                         <p className='text-2xl font-bold'>8</p>
                     </div>
-                    <div className='text-center bg-black/50 rounded-md p-2'>
-                        <p className='text-lg font-semibold'>Atendidos</p>
+                    <div className='text-center w-28 bg-purple-900/60 rounded-full p-2'>
+                        <p className='text-xs font-semibold uppercase'>Atendidos</p>
                         <p className='text-2xl font-bold'>4</p>
                     </div>
-                    <div className='text-center bg-black/50 rounded-md p-2'>
-                        <p className='text-lg font-semibold'>Total</p>
+                    <div className='text-center w-28 bg-purple-900/60 rounded-full p-2'>
+                        <p className='text-xs font-semibold uppercase'>Total</p>
                         <p className='text-2xl font-bold'>12</p>
                     </div>
                 </div>
