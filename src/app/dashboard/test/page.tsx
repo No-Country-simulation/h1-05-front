f65f68a3b30f01c54bc819604ef4c145b9a6d43e
Madays <maday.choque@gmail.com>
2024-07-25 16:09:03 -0500

    diseño responsivo

--- src/app/dashboard/test/page.tsx
+++ src/app/dashboard/test/page.tsx
@@ -2,7 +2,7 @@ import SearchVideos from '@/components/search-tutovideos'
 
 export default function TestPage() {
     return (
-        <div className='min-h-screen flex flex-col gap-4 items-center justify-start'>
+        <div className='min-h-screen flex flex-col gap-4 items-center justify-start pb-24'>
             <p className='mb-4'>Pagina de pruebas</p>
             <SearchVideos />
         </div>
